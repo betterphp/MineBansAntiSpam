01ccb5c4ef5ee1663b1439d454dbc7ea173c88ab
Jacek <jacek@kuzemczak.co.uk>
2012-08-17 17:39:42 +0100

    Removed dependency on MineBans

--- src/main/java/com/minebans/antispam/AntiSpam.java
+++ src/main/java/com/minebans/antispam/AntiSpam.java
@@ -2,25 +2,19 @@ package com.minebans.antispam;
 
 import uk.co.jacekk.bukkit.baseplugin.BasePlugin;
 
-import com.minebans.MineBans;
 import com.minebans.antispam.checks.DuplicateMessageCheck;
 import com.minebans.antispam.checks.PlayerDataChecker;
 import com.minebans.antispam.checks.PlayerLocationCheck;
 import com.minebans.antispam.data.PlayerDataListener;
-import com.minebans.pluginapi.MineBansPluginAPI;
 
 public class AntiSpam extends BasePlugin {
 	
 	public PlayerDataManager dataManager;
 	
-	public MineBansPluginAPI mineBans;
-	
 	public void onEnable(){
 		super.onEnable(false);
 		
 		this.dataManager = new PlayerDataManager();
-		
-		this.mineBans = ((MineBans) this.pluginManager.getPlugin("MineBans")).getPluginAPI(this);
 		
 		this.pluginManager.registerEvents(new PlayerDataListener(this), this);
 		this.pluginManager.registerEvents(new PlayerLocationCheck(this), this);
